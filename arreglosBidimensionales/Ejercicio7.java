6ee03dd8abdcbbe18710ac12f8e1d0e72cebe5e4
@reroes <reroes799@gmail.com>
2019-12-19 10:25:08 -0500

    manejo de archivo bidimensionales

---
+++ arreglosBidimensionales/Ejercicio7.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication76;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejercicio7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int[][] arreglo = new int[2][3];
+        
+        for(int fila=0; fila<arreglo.length; fila++){
+            
+            for(int columna=0; columna<arreglo[fila].length; columna++){
+                System.out.printf("[%d,%d]\t%d\n", fila, columna, arreglo[fila][columna]);
+            }    
+            System.out.println("--------------------------------");
+            
+        }
+        
+        
+        
+        
+    }
+    
+}
